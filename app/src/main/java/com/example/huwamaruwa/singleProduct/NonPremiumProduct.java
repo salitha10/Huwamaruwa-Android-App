dcd6cf9f65a40c5537e7a06ec5a2c7643153a214
Danuja Wijerathna <danujawijerathne45@gmail.com>
2021-04-28 01:08:02 +0530

    make nonpremium interface and half of payment option

---
+++ app/src/main/java/com/example/huwamaruwa/singleProduct/NonPremiumProduct.java
@@ -0,0 +1,16 @@
+package com.example.huwamaruwa.singleProduct;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.huwamaruwa.R;
+
+public class NonPremiumProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_premium_product);
+    }
+}
---
+++ app/src/main/java/com/example/huwamaruwa/singleProduct/PremiumProduct.java
@@ -0,0 +1,16 @@
+package com.example.huwamaruwa.singleProduct;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.huwamaruwa.R;
+
+public class PremiumProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premium_product);
+    }
+}
