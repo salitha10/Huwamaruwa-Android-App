3dbcc48c217e7bc58242cbbc8762cc2ca0598a85
Esala Senarathna <esalakalshan@gmail.com>
2021-05-02 13:49:06 +0530

    update

--- app/src/main/java/com/example/huwamaruwa/buyerRentalRequestManage/BuyerRentalRequest.java
+++ app/src/main/java/com/example/huwamaruwa/buyerRentalRequestManage/BuyerRentalRequest.java
@@ -86,6 +86,7 @@ public class BuyerRentalRequest extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Failed adding the request", Toast.LENGTH_SHORT).show();
 
         }
-    }
 
+
+    }
 }
