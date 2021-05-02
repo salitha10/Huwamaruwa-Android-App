27684578912d8084825b06a6334a83f771650b88
Esala Senarathna <esalakalshan@gmail.com>
2021-05-02 10:24:18 +0530

    update

---
+++ app/src/main/java/com/example/huwamaruwa/BuyerRentalRequest.java
@@ -0,0 +1,14 @@
+package com.example.huwamaruwa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyerRentalRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_rental_request);
+    }
+}
--- app/src/main/java/com/example/huwamaruwa/SignUp.java
+++ app/src/main/java/com/example/huwamaruwa/SignUp.java
@@ -6,15 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
-    public void signUp(View view){
-        Intent intent = new Intent(this,SignUp.class);
+    public void signIn(View view){
+        Intent intent = new Intent(this,Login.class);
         startActivity(intent);
+
     }
 }
