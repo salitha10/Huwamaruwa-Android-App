8390e8b840e0db34c632ddbbe737c871f759078a
Danuja Wijerathna <danujawijerathne45@gmail.com>
2021-05-02 19:23:53 +0530

    make temp product add

---
+++ app/src/main/java/com/example/huwamaruwa/Models/Product.java
@@ -0,0 +1,71 @@
+package com.example.huwamaruwa.Models;
+
+public class Product {
+    private String price;
+    private String title;
+    private String description;
+    private String images1;
+    private String images2;
+    private String images3;
+    private String images4;
+
+    public Product() {
+    }
+
+
+    public String getImages1() {
+        return images1;
+    }
+
+    public void setImages1(String images1) {
+        this.images1 = images1;
+    }
+
+    public String getImages2() {
+        return images2;
+    }
+
+    public void setImages2(String images2) {
+        this.images2 = images2;
+    }
+
+    public String getImages3() {
+        return images3;
+    }
+
+    public void setImages3(String images3) {
+        this.images3 = images3;
+    }
+
+    public String getImages4() {
+        return images4;
+    }
+
+    public void setImages4(String images4) {
+        this.images4 = images4;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
