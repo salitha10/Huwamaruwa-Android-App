b2063d2b0abdefddbc3d041a42bc86a61ecc71cf
Esala Senarathna <esalakalshan@gmail.com>
2021-05-04 00:13:21 +0530

    SignUp

--- app/src/main/java/com/example/huwamaruwa/buyerRentalRequestManage/BuyerRentalRequest.java
+++ app/src/main/java/com/example/huwamaruwa/buyerRentalRequestManage/BuyerRentalRequest.java
@@ -7,7 +7,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.huwamaruwa.R;
@@ -32,6 +31,7 @@ public class BuyerRentalRequest extends AppCompatActivity {
         quantity = (EditText)findViewById(R.id.productQuantityEdit);
         requiredDate = (EditText)findViewById(R.id.requiredDateEdit);
         budget = (EditText)findViewById(R.id.budgetEdit);
+
         cancel =findViewById(R.id.buyerReqCancel);
         submit =findViewById(R.id.buyerReqsubmit);
 
@@ -45,55 +45,6 @@ public class BuyerRentalRequest extends AppCompatActivity {
         quantity.setText("");
         requiredDate.setText("");
         budget.setText("");
-<<<<<<< HEAD
-    }
-
-    public void Save(View v){
-        try {
-            dbf = FirebaseDatabase.getInstance().getReference().child("BuyerRentalRequest");
-
-            if (TextUtils.isEmpty(productTitle.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "Text Field is empty", Toast.LENGTH_SHORT).show();
-            }
-            if (TextUtils.isEmpty(category.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "Text Field is empty", Toast.LENGTH_SHORT).show();
-            }
-            if (TextUtils.isEmpty(quantity.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "Text Field is empty", Toast.LENGTH_SHORT).show();
-            }
-            if (TextUtils.isEmpty(requiredDate.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "Text Field is empty", Toast.LENGTH_SHORT).show();
-            }
-            if (TextUtils.isEmpty(budget.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "Text Field is empty", Toast.LENGTH_SHORT).show();
-            }
-            else{
-
-                buyerRentalRequestsModel.setProductTitle(productTitle.getText().toString().trim());
-                buyerRentalRequestsModel.setCategory(category.getText().toString().trim());
-                buyerRentalRequestsModel.setQuantity(quantity.getText().toString().trim());
-                buyerRentalRequestsModel.setRequiredDate(requiredDate.getText().toString().trim());
-                buyerRentalRequestsModel.setBudget(budget.getText().toString().trim());
-                buyerRentalRequestsModel.setBuyerRequestId(dbf.push().getKey());
-
-
-//                dbf.push().setValue(buyerRentalRequestsModel);
-                dbf.child(buyerRentalRequestsModel.getBuyerRequestId()).setValue(buyerRentalRequestsModel);
-
-                clearContent();
-
-                Toast.makeText(getApplicationContext(), "Request added successfully", Toast.LENGTH_LONG).show();
-            }
-
-
-        }catch (Exception e){
-            Toast.makeText(getApplicationContext(), "Failed adding the request", Toast.LENGTH_SHORT).show();
-
-        }
-
-
-=======
->>>>>>> c50e1ad (Recycler View added)
     }
 
     public void Save(View v){
