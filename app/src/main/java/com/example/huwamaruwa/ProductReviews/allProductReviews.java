2140b1ba284e233ce0bdf407fd5b0ba5b9f3ef39
salitha10 <salithaniranjana@gmail.com>
2021-05-04 19:32:37 +0530

    allReviews

--- app/src/main/java/com/example/huwamaruwa/ProductReviews/allProductReviews.java
+++ app/src/main/java/com/example/huwamaruwa/ProductReviews/allProductReviews.java
@@ -1,6 +1,10 @@
 package com.example.huwamaruwa.ProductReviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import com.example.huwamaruwa.R;
 
@@ -10,5 +14,15 @@ public class allProductReviews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_product_reviews);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft1 = fm.beginTransaction();
+        ft1.add(R.id.myReviewLayout, new myReviewFragment());
+        ft1.commit();
+
+        FragmentTransaction ft2 = fm.beginTransaction();
+        ft2.add(R.id.allReviewLayout, new OtherReviewsFragment());
+        ft2.commit();
+
     }
 }
