fc4411ec29f8d71e530c401d2441dc776ce50396
danuja-wije <61626598+danuja-wije@users.noreply.github.com>
2021-05-06 15:10:48 +0530

    Danuja pro (#27)
    * make crud operation to request Rent
    
    * make bug fix of add product and home animation
    
    * floting navigation button bug
    
    * fix home floating button bug and side nav
    
    * fix floating layout
    
    * make time management
    
    Co-authored-by: Salitha Tennakoon <salithaniranjana@gmail.com>

--- app/src/main/java/com/example/huwamaruwa/addProduct/AddNewItem.java
+++ app/src/main/java/com/example/huwamaruwa/addProduct/AddNewItem.java
@@ -8,12 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
@@ -25,7 +22,6 @@ import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import com.example.huwamaruwa.MainActivity;
 import com.example.huwamaruwa.Models.Product;
 
 import com.example.huwamaruwa.Progress.LoadingProgress;
@@ -33,8 +29,6 @@ import com.example.huwamaruwa.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -43,13 +37,13 @@ import com.google.firebase.storage.UploadTask;
 import com.sangcomz.fishbun.FishBun;
 import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+
 
 public class AddNewItem extends AppCompatActivity {
 
@@ -68,7 +62,6 @@ public class AddNewItem extends AppCompatActivity {
     LoadingProgress loadingProgress;
     String imgData[];
     RadioButton RentperDay_radio,RentperHour_radio;
-    String userId;
 
     int i=0;
 
@@ -89,10 +82,7 @@ public class AddNewItem extends AppCompatActivity {
         edtminRentTime=(EditText)findViewById(R.id.minDate);
         swhAddpost=(Switch)findViewById(R.id.swhAddpost);
 
-        //getting current user
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        Log.e("login",user.getUid());
-        userId = user.getUid();
+
 
         img_list = new ArrayList<>();
         imgData = new String[4];
@@ -127,11 +117,6 @@ public class AddNewItem extends AppCompatActivity {
         autoCompleteTextloc.setAdapter(arrayAdapter2);
 
 
-    }
-
-    @Override
-    public void onBackPressed() {
-        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
@@ -215,12 +200,15 @@ public class AddNewItem extends AppCompatActivity {
 
     public void dataUploader(){
 
+
+
         post = new Product();
+
         SimpleDateFormat dateTimeInGMT = new SimpleDateFormat("dd");
         //Setting the time zone
         dateTimeInGMT.setTimeZone(TimeZone.getTimeZone("GMT+5:30"));
         post.setDate_in_day(Integer.parseInt(dateTimeInGMT.format(new Date())));
-        dateTimeInGMT = new SimpleDateFormat("HH");
+        dateTimeInGMT = new SimpleDateFormat("hh");
         post.setDate_in_hour(Integer.parseInt(dateTimeInGMT.format(new Date())));
         dateTimeInGMT = new SimpleDateFormat("mm");
         post.setDate_in_min(Integer.parseInt(dateTimeInGMT.format(new Date())));
@@ -228,6 +216,7 @@ public class AddNewItem extends AppCompatActivity {
         post.setDate_in_sec(Integer.parseInt(dateTimeInGMT.format(new Date())));
         dateTimeInGMT = new SimpleDateFormat("yyy");
         post.setDate_in_year(Integer.parseInt(dateTimeInGMT.format(new Date())));
+
         Boolean isPremium = swhAddpost.isChecked();
         post.setIsPremium(isPremium);
         post.setLocation(autoCompleteTextloc.getText().toString().trim());
@@ -236,13 +225,16 @@ public class AddNewItem extends AppCompatActivity {
         post.setContactNumber(addProdcontact.getText().toString().trim());
         post.setDepositPercentage(Double.parseDouble(deposit.getText().toString().trim()));
         post.setDescription(prodDescription.getText().toString().trim());
+
         post.setImages1(imgData[0]);
         post.setImages2(imgData[1]);
         post.setImages3(imgData[2]);
         post.setImages4(imgData[3]);
+
         post.setMaxRentalTime(Integer.parseInt(edtmaxDate.getText().toString().trim()));
         post.setMinRentalTime(Integer.parseInt(edtminRentTime.getText().toString().trim()));
-        post.setSellerId(userId);
+
+
         post.setId(dbRefe.push().getKey());
         dbRefe.child(post.getId()).setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
 
@@ -267,19 +259,9 @@ public class AddNewItem extends AppCompatActivity {
         if(i >= img_list.size()){
             dataUploader();
         } else{
-            Bitmap bmp = null;
-            try {
-                bmp = MediaStore.Images.Media.getBitmap(getContentResolver(),img_list.get(i));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.JPEG, 25, baos);
-            byte[] data = baos.toByteArray();
-            //uploading the image
             StorageReference storageReference  = sdbRefe.child(System.currentTimeMillis() +"."+ GetFileExtension(img_list.get(i)));
             int imgfinal = i;
-            storageReference.putBytes(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            storageReference.putFile(img_list.get(i)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     Toast.makeText(AddNewItem.this, "image "+ (imgfinal +1)+" uploaded", Toast.LENGTH_SHORT).show();
@@ -340,6 +322,7 @@ public class AddNewItem extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == RESULT_OK) {
 
+            Toast.makeText(this, "Multiple Image selected", Toast.LENGTH_SHORT).show();
             img_list = data.getParcelableArrayListExtra(FishBun.INTENT_PATH);
 
             initRecycler();
@@ -352,5 +335,4 @@ public class AddNewItem extends AppCompatActivity {
         recyclerView.setAdapter(postAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
     }
-
 }
