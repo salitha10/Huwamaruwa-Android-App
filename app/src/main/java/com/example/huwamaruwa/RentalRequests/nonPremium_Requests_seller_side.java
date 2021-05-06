7ae01f00f688730cf18fe7207f93524962218441
danuja-wije <61626598+danuja-wije@users.noreply.github.com>
2021-05-06 15:10:48 +0530

    Danuja pro (#27)
    * make crud operation to request Rent
    
    * make bug fix of add product and home animation
    
    * floting navigation button bug
    
    * fix home floating button bug and side nav
    
    * fix floating layout
    
    * make time management
    
    Co-authored-by: Salitha Tennakoon <salithaniranjana@gmail.com>

---
+++ app/src/main/java/com/example/huwamaruwa/RentalRequests/nonPremium_Requests_seller_side.java
@@ -0,0 +1,30 @@
+package com.example.huwamaruwa.RentalRequests;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.huwamaruwa.R;
+
+public class nonPremium_Requests_seller_side extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_non_premium__requests_seller_side, container, false);
+
+
+
+
+
+
+
+
+        return view;
+    }
+}
