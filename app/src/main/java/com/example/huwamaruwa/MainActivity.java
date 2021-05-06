19b2db27d938473f6f5e337d59eaa4233edb40da
danuja-wije <61626598+danuja-wije@users.noreply.github.com>
2021-05-06 21:41:15 +0530

    Danuja merge (#29)
    * make crud operation to request Rent
    
    * make bug fix of add product and home animation
    
    * floting navigation button bug
    
    * fix home floating button bug and side nav
    
    * fix floating layout
    
    * make time management
    
    * request rent management seller side & admin side.
    
    * make admin&seller rental request
    
    Co-authored-by: Salitha Tennakoon <salithaniranjana@gmail.com>

--- app/src/main/java/com/example/huwamaruwa/MainActivity.java
+++ app/src/main/java/com/example/huwamaruwa/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.huwamaruwa;
 
-
 import android.content.Context;
 
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
-import android.annotation.SuppressLint;
-
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -41,7 +35,6 @@ import com.example.huwamaruwa.R;
 import com.example.huwamaruwa.RentalRequests.PremiumProductRentalRequestFragment;
 import com.example.huwamaruwa.RentalRequests.nonPremium_Requests_seller_sideFragment;
 import com.example.huwamaruwa.addProduct.AddNewItem;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -78,24 +71,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     FloatingActionButton floatingActionButton_add;
     FloatingActionButton floatingActionButton_req;
     TextView txtFloatingAdd,txtFloatingReq;
-
     ConstraintLayout floatingSheet;
-
     Animation rotateOpenAnim;
     Animation rotateCloseAnim;
     Animation fromBottomAnim;
     Animation toBottomAnim;
-
     Animation bottomSheet;
     Animation topSheet;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         //get values by id
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
@@ -109,9 +101,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         floatingActionButton_req = findViewById(R.id.floating_buyer_req_button);
         txtFloatingAdd = findViewById(R.id.txtfloating_add);
         txtFloatingReq = findViewById(R.id.txtfloating_req);
-
         floatingSheet = findViewById(R.id.floating_bottom_sheet);
-
         //set app name to toolbar
        // setSupportActionBar(toolbar);
 
@@ -172,7 +162,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 rotateCloseAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_closefloating_anim);
                 fromBottomAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.from_bottom_floating_anim);
                 toBottomAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.to_bottom_floating_anim);
-
                 bottomSheet = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.floating_bottom_top_animation);
                 topSheet = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.floating_bottom_bottom_animation);
                 setVisibility(clicker);
